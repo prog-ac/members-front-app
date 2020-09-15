6a179a9c7c657ab005272ea6078a6271c5b4e26c
ide Shouhi <shouhi0922@icloud.com>
2020-09-15 18:36:04 +0900

    デプロイ修正

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
-require("dotenv").config();
 module.exports = {
   env: {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    apiKey: "AIzaSyDvz8zkOIGo353QoNo7QOzcbNZ8faDqyuk",
+    authDomain: "prog-ac-members.firebaseapp.com",
+    databaseURL: "https://prog-ac-members.firebaseio.com",
+    projectId: "prog-ac-members",
+    storageBucket: "prog-ac-members.appspot.com",
+    messagingSenderId: "58384803904",
+    appId: "1:58384803904:web:e9d816bb917b62e2cb17a5",
+    measurementId: "G-J7PYN1GYC5",
   },
 };
